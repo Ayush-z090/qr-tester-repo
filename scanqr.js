be567116c2b4048e0811b03642bb242a209a4016
Ayush-z090 <semwalmeena5@gmail.com>
2025-02-20 21:08:02 +0530

    test 3

--- scanqr.js
+++ scanqr.js
@@ -1,21 +1,10 @@
-// const userData = {
-//     name: "John Doe",
-//     email: "john@example.com",
-//     userId: "12345" // Unique user ID
-// };
+let scanField = document.querySelector("#reader");
+let html5QrCode;
 
-// document.getElementById("scanBtn").addEventListener("click", () => {
-    
-//     document.getElementById("reader").style.background="white";
-
-//     document.getElementById("reader__dashboard")
-//     const scanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
-
-//     scanner.render((decodedText) => {   
-//         alert("Scanned QR Code: " + decodedText);
-
-//         // Send user data + scanned QR code to server
-//         fetch("https://your-server.com/api/store-scan", { // Replace with your backend
+const qrCodeSuccessCallback_E = (decodedText, decodedResult) => {
+    /* handle success */
+    alert("decoded test")
+    //         fetch("https://your-server.com/api/store-scan", { // Replace with your backend
 //             method: "POST",
 //             headers: { "Content-Type": "application/json" },
 //             body: JSON.stringify({ 
@@ -31,16 +20,6 @@
 //             alert("Error sending data: " + error);
 //         });
 
-//         scanner.clear(); // Stop scanner after one scan
-//         document.getElementById("reader").style.display ="none"
-
-//     })
-
-let scanField = document.querySelector("#reader");
-
-const qrCodeSuccessCallback_E = (decodedText, decodedResult) => {
-    /* handle success */
-    alert("decoded test")
 };
 
 const qrCodeSuccessCallback_M = (decodedText, decodedResult) => {
@@ -49,15 +28,26 @@ const qrCodeSuccessCallback_M = (decodedText, decodedResult) => {
 };
 
 function CreateQRScanElemnt(elemnt,succsesCallback){
-    const html5QrCode = new Html5Qrcode(elemnt);
+    scanField.style.display="block"
+    if (html5QrCode) html5QrCode.resume()
+    html5QrCode = new Html5Qrcode(elemnt);
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
     html5QrCode.start({ facingMode: "environment" }, config, succsesCallback);
     document.addEventListener("click",(eve)=>{
-        if(eve.target.matches(".stopScan")){
-            html5QrCode.stop()
+        
+        if(eve.target.matches(".stopScan") ){
+            if(eve.target.innerText==="Stop Scan")
+            {
+                html5QrCode.pause();
+                eve.target.innerText="resume scan"
+            }
+            else{
+                html5QrCode.resume()
+                eve.target.innerText="stop scan"
+            }
         }
+        
     })
-    // html5QrCode.stop()                
 }
 
 function elemntCreate(tag,className,text){
@@ -67,16 +57,17 @@ function elemntCreate(tag,className,text){
     return bit
 }
 
+let currentHrs = new Date().getHours()
+
 document.addEventListener("click",(eve)=>{
 
-    if(eve.target.matches("#M-Present")){
-
+    if(eve.target.matches("#M-Present") && currentHrs>=10){
             CreateQRScanElemnt("reader",qrCodeSuccessCallback_M)
             scanField.appendChild(elemntCreate("button","stopScan","stop scan"))
 
     }
 
-    if(eve.target.matches("#E-Present")){
+    if(eve.target.matches("#E-Present") && currentHrs>=14 ){
         
         CreateQRScanElemnt("reader",qrCodeSuccessCallback_E)
         scanField.appendChild(elemntCreate("button","stopScan","stop scan"))
